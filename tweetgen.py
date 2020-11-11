ff8cc1bbd6eb153ceb7f204e42745915811fd998
Justin Charity <66874949+bttrsctchcld@users.noreply.github.com>
2020-11-11 17:13:58 -0500

    genxpr
    refactored into smaller functions; rewrote lists as generators

--- tweetgen.py
+++ tweetgen.py
@@ -21,34 +21,31 @@ class Tweet:
     
     def get_unique_screennames(self):
         self.load_tweets()
-        return list(set([tweet["name"] for tweet in self.hashtag]))
-        
-    def get_user(self,users):
-        users = self.get_unique_screennames()
-        for user in users:
+        screennames = list(set([tweet["name"] for tweet in self.hashtag]))
+        for user in screennames:
             yield user
 
-    def update_user(self,user):
-        user = get_user()
-        self.profile["name"] = user
-        self.users.append(self.profile)
-        return
+    def update_user(self):
+        for user in self.get_unique_screennames():
+            self.profile["name"] = user
+            self.users.append(copy.copy(self.profile))
 
-    def get_profile(self,user):
-        for profile in tqdm(self.users):
-            yield profile
-
-    def match_updated_user(self,profile):
-        profile = get_profile()
-        for tweet in self.hashtag:
-            if profile["name"] == tweet["name"]:
-                profile["count"] += 1
-                if profile["profile"] == None:
-                    profile["profile"] = tweet["profile"]
+    def match_updated_user(self):
+        profiles = (profile for profile in self.users)
+        for profile in tqdm(profiles):
+            for tweet in self.hashtag:
+                if profile["name"] == tweet["name"]:
+                    profile["count"] += 1
+                    if profile["profile"] == None:
+                        profile["profile"] = tweet["profile"]
         return
 
     def print_users(self):
-        pprint.pprint(sorted(self.users,key=lambda n: n["count"],reverse=True[0:19]))
+        pprint.pprint(sorted(self.users,key=lambda n: n["count"],reverse=True)[0:19])
 
 if __name__ == "__main__":
     tweets = Tweet()
+    tweets.get_unique_screennames()
+    tweets.update_user()
+    tweets.match_updated_user()
+    tweets.print_users()
